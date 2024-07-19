9d0378c6db79c0f5d7cd75a9ef89d7c8009f7049
joeycasey87 <139773651+joeycasey87@users.noreply.github.com>
2024-07-19 14:37:09 +0200

    Create {{cookiecutter.postprocessor_name}}.py

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/{{cookiecutter.postprocessor_name}}.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+One-line description of this module. E.g.
+
+This is a more extensive description (optional) that describes the readers implemented
+in this module and other relevant information.
+"""
+
+def {{cookiecutter.postprocessor_name }}_xxx(**kwargs):
+  """
+  A detailed description of the postprocessor. A minimal documentation is
+  strictly needed since the pysteps postprocessors interface expect docstrings.
+
+  For example, a documentation may look like this:
+
+  Import a precipitation field from the Awesome Bureau of Composites stored in
+  Grib format.
+
+  Parameters
+  ----------
+  parameter1: str
+    description
+    
+  Returns
+  -------
+  output: array
+    description    
+  """
+
+  output = "hello world"
+  return output
